027b8e489c10805789723d90119ae9226604fe63
Crizz <darynalsup@gmail.com>
2025-05-16 01:29:31 -0700

    Updated variables for tribe contexts

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 import time
 import asyncio
+import warnings
 import paramiko
+import discord
 from discord.ext import commands, tasks
-from arkparse import ArkTribe  # Fixed import
+from arkparse import ArkTribe
 
-# Load env vars
+# Suppress deprecation warnings (optional)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# Load environment variables
 SFTP_IP = os.environ["SFTP_IP"]
-SFTP_PORT = int(os.environ.get("SFTP_PORT", 22))
+SFTP_PORT = int(os.environ.get("SFTP_PORT", "22"))
 SFTP_USER = os.environ["SFTP_USER"]
 SFTP_PASSWORD = os.environ["SFTP_PASSWORD"]
 LOG_PATH = os.environ["LOG_PATH"]
@@ -15,11 +20,11 @@ DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
 CHANNEL_ID = int(os.environ["CHANNEL_ID"])
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-# Local temp file
+# Local temp file for download
 LOCAL_FILE = "temp.arktribe"
 seen_logs = set()
 
-intents = commands.Intents.default()
+intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 def is_dino_death(msg):
@@ -44,7 +49,7 @@ def get_new_dino_deaths():
     deaths = []
     try:
         with open(LOCAL_FILE, "rb") as f:
-            tribe = ArkTribe(f)  # Use correct class name
+            tribe = ArkTribe(f)
         for entry in tribe.log:
             msg = getattr(entry, "message", str(entry))
             if msg not in seen_logs:
